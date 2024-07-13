f505e4e7aaae9a0d164c99b9c510042ef28c5572
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-07-13 00:45:42 -0400

    parse possible locations

--- convert.py
+++ convert.py
@@ -1,9 +1,21 @@
 import json
 import pandas
+from pprint import pprint
 
+# load data
 excel_data_df = pandas.read_excel('combined.xlsx', sheet_name='Sheet1')
-thisisjson = excel_data_df.to_json(orient='records')
-thisisjson_dict = json.loads(thisisjson)
+json_string = excel_data_df.to_json(orient='records')
+json_list = json.loads(json_string)
 
-with open('data.json', 'w') as json_file:
-    json.dump(thisisjson_dict, json_file)
+output = []
+
+for field in json_list:
+    pl = field["Possible Locations"].split(",")
+    if len(pl) == 1:
+        output.append(field)
+
+pprint(output)
+
+# write data to file
+with open('expirmental_data.json', 'w') as json_file:
+    json.dump(output, json_file)
