0005c35b4df5410b3aaaae2c9121b9b254e91b9c
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-07-13 01:24:42 -0400

    add black, remove initial spaces, blacken

--- convert.py
+++ convert.py
@@ -3,8 +3,8 @@ import pandas
 from pprint import pprint
 
 # load data
-excel_data_df = pandas.read_excel('combined.xlsx', sheet_name='Sheet1')
-json_string = excel_data_df.to_json(orient='records')
+excel_data_df = pandas.read_excel("combined.xlsx", sheet_name="Sheet1")
+json_string = excel_data_df.to_json(orient="records")
 json_list = json.loads(json_string)
 
 output = []
@@ -14,6 +14,10 @@ for field in json_list:
     pl = field["Possible Locations"].split(",")
     if len(pl) > 1:
         for location in pl:
+
+            # remove initial spaces
+            if location[:1] == " ":
+                location = location[1:]
 
             # create field "PossibleLocations" and remove "Possible Locations"
             field["PossibleLocations"] = location
@@ -28,6 +32,7 @@ for field in json_list:
         field["PossibleLocations"] = field.pop("Possible Locations")
         output.append(field)
 
+
 # write data to file
-with open('expirmental_data.json', 'w') as json_file:
+with open("expirmental_data.json", "w") as json_file:
     json.dump(output, json_file)
